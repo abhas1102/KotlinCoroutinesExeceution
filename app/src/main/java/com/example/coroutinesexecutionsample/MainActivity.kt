33e4de7be256fba5c7e66b6a359a8198cd002cbe
Abhas Kumar <abhas1102@gmail.com>
2021-09-09 15:45:46 +0530

    Worked around suspend function and called it in Coroutine scope

--- app/src/main/java/com/example/coroutinesexecutionsample/MainActivity.kt
+++ app/src/main/java/com/example/coroutinesexecutionsample/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
@@ -15,10 +16,28 @@ class MainActivity : AppCompatActivity() {
 
         //GlobalScope means coroutines started under this scope will live as long our application does
         GlobalScope.launch {
-            Log.d(TAG,"Coroutine says hello from thread ${Thread.currentThread().name}")
+           // delay(3000L)
+          //  Log.d(TAG,"Coroutine says hello from thread ${Thread.currentThread().name}")
+            val networkCall1=doNetworkCall()
+            val networkCall2=doNetworkCall2()
+
+            Log.d(TAG,networkCall1)
+            Log.d(TAG,networkCall2)
         }
 
-        Log.d(TAG,"Coroutine says hello from thread ${Thread.currentThread().name}")
+       // Log.d(TAG,"Coroutine says hello from thread ${Thread.currentThread().name}")
 
     }
+    suspend fun doNetworkCall():String{
+        delay(3000L)
+        return "This is the answer"
+
+    }
+    //First delay call will also influence the second delay call because they are executed in same coroutine
+    suspend fun doNetworkCall2():String{
+        delay(3000L)
+        return "This is my answer"
+    }
+
+
 }
